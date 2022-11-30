d385e32de6ce5f35302f043c683d8613ddb74a1a
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-30 22:12:02 +0330

    last

--- color_trio.py
+++ color_trio.py
@@ -16,14 +16,20 @@ def color_trio(colors):
                                         '').replace(new_colors[i+1], '')
         else:
             new_colors = output
-    return ''.join(output)
+    return ''.join(new_colors)
 
 
 class TestColor(unittest.TestCase):
     def test_color(self):
-        self.assertEqual(color_trio(), second)
+        self.assertEqual(color_trio('y'), 'y')
+        self.assertEqual(color_trio('bb'), 'b')
+        self.assertEqual(color_trio('rybyry'), 'r')
+        self.assertEqual(color_trio('brybbr'), 'r')
+        self.assertEqual(color_trio('rbyryrrbyrbb'), 'y')
+        self.assertEqual(color_trio('yrbbbbryyrybb'), 'b')
 
 
+unittest.main()
 # def colour_trio(colours):
 #     # runs until colours contains only one singleton element
 #     while len(colours) > 1:
