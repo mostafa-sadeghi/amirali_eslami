b124ff723284e0cdadf54c59c4644ad506a40eac
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-22 21:58:01 +0330

    1dey end

--- about_sqlite_db.py
+++ about_sqlite_db.py
@@ -15,3 +15,11 @@ print(type(movies))
 
 # connection = sqlite3.connect("movies.sqlite3")
 with sqlite3.connect("movies.sqlite3") as conn:
+    cursor = conn.cursor()
+    command = """CREATE TABLE IF NOT EXISTS MOVIES(id INT,
+    title VARCHAR(30),
+    year INT
+        )
+        """
+    cursor.execute(command)
+#     command = "INSERT INTO "
